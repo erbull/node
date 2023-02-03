7e63e97e7d2f8ff67a4ca4ddd3007b2e9c41e6e5
Erdoğan Bülbül <58750760+erbull@users.noreply.github.com>
2023-02-04 00:02:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+//
+var fs = require("fs");
+var express = require("express");
+var path = require('path');
+var app = express();
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(rq, rs){
+    fs.readFile("index.html", function(err, data){
+        rs.write(data);
+        rs.end();
+    });
+});
+
+app.listen(80);
