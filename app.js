e726cd9efb7815509f2da97bd37135fa2f6df0fd
Erdoğan Bülbül <58750760+erbull@users.noreply.github.com>
2023-02-03 19:37:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    fs.readFile('index.html', function(err, data){
+        res.write(data);
+        res.end("bitti");
+    })
+});
+
+server.listen(8000);
