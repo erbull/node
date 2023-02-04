ea136e9ecef16f186c166f6bffcacb42d8c6a439
Erdoğan Bülbül <58750760+erbull@users.noreply.github.com>
2023-02-04 14:37:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //
-var http = require('http');
+//var http = require('http');
 var fs = require('fs');
-//var express = require("express");
-//var path = require('path');
-//var app = express();
-//app.use('/public', express.static(path.join(__dirname, 'public')));
-var server = http.createServer(function(rq, rs){
-    rs.write("Mehaba");
-    rs.end();
-});
-
-//app.get("/", function(rq, rs){
-//    fs.readFile("index.html", function(err, data){
-//        rs.write(data);
-//        rs.end();
-//    });
+var express = require("express");
+var path = require('path');
+var app = express();
+app.use('/public', express.static(path.join(__dirname, 'public')));
+//var server = http.createServer(function(rq, rs){
+//    rs.write("Mehaba");
+//    rs.end();
 //});
+
+app.get("/", function(rq, rs){
+    fs.readFile("index.html", function(err, data){
+        rs.write(data);
+        rs.end();
+    });
+});
 
 //server.listen(5000);
 server.listen(process.env.PORT);
