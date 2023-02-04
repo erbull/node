65e6b1f5eba842579e1087289f1e6e242126657d
Erdoğan Bülbül <58750760+erbull@users.noreply.github.com>
2023-02-04 16:05:40 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,22 +1,10 @@
 //
 var http = require('http');
 var fs = require('fs');
-//var express = require("express");
-//var path = require('path');
-//var app = express();
-//app.use('/public', express.static(path.join(__dirname, 'public')));
+
 var server = http.createServer(function(rq, rs){
     rs.write(rq);
     rs.end();
 });
 
-//app.get('/', (rq, rs) => {
-    //fs.readFile("index.html", function(err, data){
-    //    rs.write(data);
-    //    rs.end();
-    //});
-    //rs.write('hello world')
-//});
-
-//server.listen(5000);
 server.listen(process.env.PORT || 8080);
