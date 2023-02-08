a9b97a16c71ca27007f04f3de0cab1c99cbb5a8b
Erdoğan Bülbül <58750760+erbull@users.noreply.github.com>
2023-02-08 14:51:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 //
-
-var fs = require('fs');
 var express = require("express");
-var path = require('path');
 var app = express();
+var path = require('path');
+var router = express.Router();
+app.set('view engine', 'ejs');
+var ejsLayout = require('express-ejs-layouts');
+
+var homeController = require('./controllers/HomeController');
+var computerController = require('./controllers/ComputerController');
+var fundamentalsController = require('./controllers/FundamentalsController');
+var examplesController = require('./controllers/ExamplesController');
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(ejsLayout);
+app.use("/", homeController);
+app.use("/computer", computerController);
+app.use("/programming/fundamentals", fundamentalsController);
+app.use("/examples", examplesController)
 
-
-app.get('/', (rq, rs) => {
-    fs.readFile("index.html", function(err, data){
-        rs.write(data);
-        rs.end();
-    });
-});
-
-//server.listen(5000);
 app.listen(process.env.PORT || 8080);
